7f8cea7cc49284e31bb85ab46909022bd3ef3284
Hải Nguyễn <166189486+20210313-haind@users.noreply.github.com>
2024-04-24 21:19:37 +0700

    init commit

---
+++ frontend/vite-project/src/components/Login/Login.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Login.css'
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Username:', username);
+    console.log('Password:', password);
+    setUsername('');
+    setPassword('');
+  };
+
+  return (
+    <div className='login-container'>
+      <h2>Đăng nhập</h2>
+      <form onSubmit={handleSubmit}>
+        <div className='input-group'>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={handleUsernameChange}
+          />
+        </div>
+        <div className='input-group'>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <button className='submit-btn' type="submit">Đăng nhập</button>
+      </form>
+      <div className='link-register'>
+        Chưa có tài khoản? <Link to="/register">Đăng ký</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
