48f9186ce0de305727f26ffbe74982cd4dd6b33f
Hải Nguyễn <166189486+20210313-haind@users.noreply.github.com>
2024-05-19 11:03:08 +0700

    update

---
+++ frontend/vite-project/src/layouts/auth/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Container, Stack } from "@mui/material";
+import { Navigate, Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+const AuthLayout = () => {
+  const { isLoggedIn } = useSelector((state) => state.auth);
+
+  if (isLoggedIn) {
+    return <Navigate to={"/app"} />;
+  }
+
+  return (
+    <>
+      <Container sx={{ mt: 5 }} maxWidth="sm">
+        <Stack spacing={5}>
+          <Stack
+            sx={{ width: "100%" }}
+            direction="column"
+            alignItems={"center"}
+          >
+          </Stack>
+          <Outlet />
+        </Stack>
+      </Container>
+    </>
+  );
+};
+
+export default AuthLayout;
